974d7cb0ab9140893d386605a33cb30a2605c91b
Ullugbek <okabdulaz@gmail.com>
2025-01-08 10:53:27 +0500

    first commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new MyBot("6527912497:AAEkVSwvFzM0rSI9vZ_i5r5-frFsUuXyTDE"));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
